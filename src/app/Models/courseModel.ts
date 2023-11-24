b0ba3c4fbbc53885895882ae5ae5a542ec532e65
Divya Ratra <divyaratra1999@gmail.com>
2023-11-24 14:40:52 -0500

    API called for getAllCourses and some css changes

--- src/app/Models/courseModel.ts
+++ src/app/Models/courseModel.ts
@@ -1,6 +1,23 @@
-// src/app/course.model.ts
+export class InstructorDetails {
+  encPassword?: string;
+  password?: string;
+  roleId?: number;
+  name?: string;
+  roleName?: string;
+  id?: number;
+  email?: string;
+}
 
 export class CourseModel {
-    constructor(public id:number,public title: string, public description: string) {}
+  id?: number;
+  title?: string;
+  instructorId?: number;
+  description?: string;
+  avgDuration?: number;
+  difficulty?: string;
+  instructorDetails?: InstructorDetails;
+
+  constructor(data: any) {
+    Object.assign(this, data);
   }
-  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { SignupComponent } from './component/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignupService } from './services/signup.service';
 import { LoginComponent } from './component/login/login.component';
-import { DashboardInstructorComponent } from './dashboard-instructor/dashboard-instructor.component';
+// import { DashboardInstructorComponent } from './dashboard-instructor/dashboard-instructor.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 import { StudentCourseViewComponent } from './component/student-course-view/student-course-view.component';
 import { StudentDashboardComponent } from './component/student-dashboard/student-dashboard.component';
@@ -18,7 +18,7 @@ import { StudentDashboardComponent } from './component/student-dashboard/student
     AppComponent,
     SignupComponent,
     LoginComponent,
-    DashboardInstructorComponent,
+    // DashboardInstructorComponent,
     CourseDetailsComponent,
     StudentCourseViewComponent,
     StudentDashboardComponent
