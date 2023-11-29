9c53ed994b99b6837b98ca8a8c609963e9cbf203
Jayesh Pamnani <jayeshpamnani15@gmail.com>
2023-11-29 02:21:00 -0500

    Adding Test Cases

--- src/app/component/teacher-dashboard/teacher-dashboard.component.spec.ts
+++ src/app/component/teacher-dashboard/teacher-dashboard.component.spec.ts
@@ -1,21 +1,133 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+// Import necessary modules and classes
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { TeacherDashboardComponent } from './teacher-dashboard.component';
+import { CourseService } from 'src/app/services/course.service';
+import { SignupConfirmationDialogComponent } from '../signup-confirmation-dialog/signup-confirmation-dialog.component';
+import { LocalStorageService } from 'src/app/local-storage.service';
+import { of } from 'rxjs';
+import { Router as AngularRouter } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { waitForAsync } from '@angular/core/testing';
 
 describe('TeacherDashboardComponent', () => {
   let component: TeacherDashboardComponent;
   let fixture: ComponentFixture<TeacherDashboardComponent>;
+  let mockCourseService: jasmine.SpyObj<CourseService>;
+  let mockDialog: jasmine.SpyObj<MatDialog>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockLocalStorage: jasmine.SpyObj<LocalStorageService>;
 
   beforeEach(() => {
+    // ... (Same setup as before)
+
     TestBed.configureTestingModule({
-      declarations: [TeacherDashboardComponent]
+      declarations: [TeacherDashboardComponent],
+      providers: [
+        { provide: CourseService, useValue: mockCourseService },
+        { provide: MatDialog, useValue: mockDialog },
+        { provide: Router, useValue: mockRouter },
+        { provide: LocalStorageService, useValue: mockLocalStorage }
+      ]
     });
+
     fixture = TestBed.createComponent(TeacherDashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+  it('should show all courses when calling showAllCourses()', () => {
+    component.viewAllCourses = false;
+    component.loadAllCourses = jasmine.createSpy('loadAllCourses');
+
+    component.showAllCourses();
+
+    expect(component.viewAllCourses).toBeTrue();
+    expect(component.loadAllCourses).toHaveBeenCalled();
+    expect(component.buttonType).toBe('Enroll');
+  });
+
+  // ...
+
+  // describe('TeacherDashboardComponent', () => {
+  //   let component: TeacherDashboardComponent;
+  //   let fixture: ComponentFixture<TeacherDashboardComponent>;
+  //   let mockCourseService: jasmine.SpyObj<CourseService>;
+  //   let mockDialog: jasmine.SpyObj<MatDialog>;
+  //   let mockRouter: jasmine.SpyObj<Router>;
+
+  //   // ...
+
+  //   beforeEach(waitForAsync(() => {
+  //     // ...
+
+  //     TestBed.configureTestingModule({
+  //       declarations: [TeacherDashboardComponent],
+  //       providers: [
+  //         { provide: CourseService, useValue: mockCourseService },
+  //         { provide: MatDialog, useValue: mockDialog },
+  //         { provide: Router, useValue: mockRouter },
+  //         { provide: LocalStorageService, useValue: mockLocalStorage }
+  //       ],
+  //       imports: [
+  //         HttpClientTestingModule, // Provide HttpClient for testing
+  //         RouterTestingModule,     // Mocks the router
+  //         MatDialogModule,         // Mocks MatDialog
+  //         ReactiveFormsModule     // Provides FormBuilder for testing
+  //       ]
+  //     }).compileComponents();
+  //     // ...
+  //   }));
+
+  //   beforeEach(() => {
+  //     fixture = TestBed.createComponent(TeacherDashboardComponent);
+  //     component = fixture.componentInstance;
+  //     fixture.detectChanges();
+  //   });
+
+  //   // ...
+
+  //   // it('should call buttonClick method on button click', () => {
+  //   //   spyOn(component, 'buttonClick'); // Spy on the buttonClick method
+
+  //   //   const button = fixture.debugElement.nativeElement.querySelector('.course-enroll-button');
+  //   //   button.click();
+
+  //   //   expect(component.buttonClick).toHaveBeenCalled(); // Verify that the method was called
+  //   // });
+  //   // it('11111should call buttonClick method on button click', () => {
+  //   //   spyOn(component, 'buttonClick'); // Spy on the buttonClick method
+
+  //   //   const button = fixture.debugElement.nativeElement.querySelector('.course-enroll-button');
+  //   //   button.click();
+
+  //   //   expect(component.buttonClick).toHaveBeenCalledWith(1)// Verify that the method was called with the correct parameter
+  //   // });
+
+    
+
+
+  // });
+
+  // Add more test cases as needed
+
+  it('should call showAllCourses method on button click', () => {
+    spyOn(component, 'showAllCourses'); // Spy on the showAllCourses method
+
+    const button = fixture.debugElement.nativeElement.querySelector('.nav-link'); // Adjust the selector based on your actual HTML structure
+    button.click();
+
+    expect(component.showAllCourses).toHaveBeenCalled(); // Verify that the method was called
+  });
+
+  
+  
 });
--- src/app/component/view-course-module/view-course-module.component.spec.ts
+++ src/app/component/view-course-module/view-course-module.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ViewCourseModuleComponent } from './view-course-module.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import for MatDialogModule
+import { of } from 'rxjs';
 
 describe('ViewCourseModuleComponent', () => {
   let component: ViewCourseModuleComponent;
@@ -8,14 +12,70 @@ describe('ViewCourseModuleComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ViewCourseModuleComponent]
+      declarations: [ViewCourseModuleComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule, // Import MatDialogModule
+        BrowserAnimationsModule, // Import for MatDialogModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (param: string) => '1', // Adjust to match your route parameter value
+              },
+            },
+          },
+        },
+      ],
     });
+
     fixture = TestBed.createComponent(ViewCourseModuleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+  it('should display the course content iframe', fakeAsync(() => {
+    
+    spyOn((component as any).courseService, 'getModuleDetailsFull').and.returnValue(of({
+      id: 1, // Ensure 'id' property is available in the returned object
+      title: 'Test Module',
+      description: 'This is a test module',
+      serialNumber: 1,
+    }));
+
+    // ...
+  }));
+
+
+  // it('should display the module title', fakeAsync(() => {
+  //   fixture.detectChanges();
+  //   tick();
+  //   const titleElement: HTMLElement = fixture.debugElement.nativeElement.querySelector('h1');
+  //   expect(titleElement.textContent).toContain('Test Module');
+  // }));
+
+  // it('should display the module description', () => {
+  //   const descriptionElement: HTMLElement = fixture.debugElement.nativeElement.querySelector('li strong:nth-child(1)');
+  //   expect(descriptionElement.textContent).toContain('Description:');
+  //   expect(descriptionElement.textContent).toContain('This is a test module');
+  // });
+
+  // it('should display the module week number', () => {
+  //   const weekNumberElement: HTMLElement = fixture.debugElement.nativeElement.querySelector('li strong:nth-child(2)');
+  //   expect(weekNumberElement.textContent).toContain('Week Number:');
+  //   expect(weekNumberElement.textContent).toContain('1');
+  // });
+
+  // it('should display the module serial number', () => {
+  //   const serialNumberElement: HTMLElement = fixture.debugElement.nativeElement.querySelector('li strong:nth-child(3)');
+  //   expect(serialNumberElement.textContent).toContain('Serial Number:');
+  //   expect(serialNumberElement.textContent).toContain('1');
+  // });
+
+  
+
+  
 });
